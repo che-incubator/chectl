c8f8f6b0c65628488739179065749a5a4d2a778c
Mario Loriedo <mario.loriedo@gmail.com>
2018-11-24 16:48:36 +0100

    Updates to server:up

--- src/commands/server/up.ts
+++ src/commands/server/up.ts
@@ -28,19 +28,20 @@ export default class Up extends Command {
     const Listr = require('listr')
     const notifier = require('node-notifier')
     const tasks = new Listr([
-      { title: 'Verify that minikube is installed', task: () => this.checkIfInstalled('minikube') },
-      { title: 'Verify that helm is installed', task: () => this.checkIfInstalled('helm') },
-      { title: 'Verify that minikube is running', task: () => this.checkMinikubeStatus() },
-      { title: 'Verify that minikube ingress addon is enabled', task: () => this.checkIfMinikubeIngressAddon() },
-      { title: 'Verify that if ClusterRoleBinding addo-on-cluster-admin exist', task: () => this.checkClusterRoleBinding() },
+      { title: 'Verify if minikube is installed', task: (task: any) => { this.checkIfInstalled('minikube'); task.output = 'Done' } },
+      { title: 'Verify if helm is installed', task: () => this.checkIfInstalled('helm') },
+      { title: 'Verify if minikube is running', task: (ctx: any) => { ctx.minikubeIsRunning = this.isMinikubeRunning(ctx) }},
+      { title: 'Start minikube', skip: (ctx: any) => { if (ctx.minikubeIsRunning) { return 'Minikube is already running' } }, task: () => this.startMinikube() },
+      { title: 'Verify if minikube ingress addon is enabled', task: () => this.checkIfMinikubeIngressAddon() },
+      { title: 'Verify if ClusterRoleBinding add-on-cluster-admin exist', task: () => this.checkClusterRoleBinding() },
       //execSync('kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default', { timeout: 10000 })
-      { title: 'Verify that tiller service account exists', task: () => this.checkTillerServiceAccount() },
+      { title: 'Verify if Tiller Service Account exists', task: () => this.checkTillerServiceAccount() },
       // execSync('kubectl create serviceaccount tiller --namespace kube-system', { timeout: 10000 })
       { title: 'Create Tiller RBAC', task: () => this.createTillerRBAC(flags.chepath) },
-      { title: 'Verify that tiller is running', task: () => this.checkTillerService() },
+      { title: 'Verify if tiller is running', task: () => this.checkTillerService() },
       //execSync('helm init --service-account tiller', { timeout: 10000 })
       // { title: 'Deploy Che', task: () => this.deployChe(flags) },
-      { title: 'Verify that Che is running', task: () => this.checkIfCheIsRunning(flags) }
+      { title: 'Verify if Che Pod has been created', task: () => this.checkIfChePodIsCreated(flags) }
       //
       // {
       //   title: 'Verify that minikube is running',
@@ -70,51 +71,68 @@ export default class Up extends Command {
     })
   }
 
-  checkIfInstalled(commandName: string) {
-    let commandExistsSync = require('command-exists').sync
-    if (!commandExistsSync(commandName)) {
+  async sleep() {
+    await execa('sleep', ['2'])
+  }
+  // sleep(): Promise<number> {
+  //   return new Promise((resolve, _reject) => {
+  //     execa('sleep', ['2']).then(resolve(1))
+  //   })
+  // }
+
+  async checkIfInstalled(commandName: string) {
+    let commandExists = require('command-exists')
+    if (!await commandExists(commandName)) {
       throw new Error('ERROR: minikube is not installed.')
     }
   }
 
-  checkMinikubeStatus() {
-    const { stdout } = execa.shellSync('minikube status', { timeout: 10000 })
-    if (!stdout.includes('Running')) {
-      throw new Error('minikube is not running')
-    }
+  async isMinikubeRunning(ctx: any) {
+    const { stdout, code } = await execa('minikube', ['status'], { timeout: 10000, reject: false })
+    if (code === 0) { return true } else { return false }
+    // if (stdout.includes('Running')) {
+    //   // throw new Error('minikube is not running')
+    //    = true
+    // } else {
+    //   ctx.minikubeIsRunning = false
+    // }
   }
 
-  checkIfMinikubeIngressAddon() {
-    const { stdout } = execa.shellSync('minikube addons list', { timeout: 10000 })
+  async startMinikube() {
+    await execa('minikube', ['start', '--memory=4096', '--cpus=4', '--disk-size=50g'], { timeout: 120000 })
+  }
+
+  async checkIfMinikubeIngressAddon() {
+    const { stdout } = await execa('minikube', ['addons', 'list'], { timeout: 10000 })
     if (!stdout.includes('ingress: enabled')) {
       throw new Error('minikube ingress addon is not enabled')
     }
   }
 
-  checkClusterRoleBinding() {
-    execa.shellSync('kubectl get clusterrolebinding add-on-cluster-admin', { timeout: 10000 })
+  async checkClusterRoleBinding() {
+    await execa('kubectl', ['get', 'clusterrolebinding', 'add-on-cluster-admin'], { timeout: 10000 })
   }
 
-  checkTillerServiceAccount() {
-    execa.shellSync('kubectl get serviceaccounts tiller --namespace kube-system', { timeout: 10000 })
+  async checkTillerServiceAccount() {
+    await execa('kubectl', ['get', 'serviceaccounts', 'tiller', '--namespace', 'kube-system'], { timeout: 10000 })
   }
 
-  createTillerRBAC(cheLocalRepoPath: any) {
-    execa.shellSync(`kubectl apply -f ${cheLocalRepoPath}/deploy/kubernetes/helm/che/tiller-rbac.yaml`, { timeout: 10000 })
+  async createTillerRBAC(cheLocalRepoPath: any) {
+    await execa.shell(`kubectl apply -f ${cheLocalRepoPath}/deploy/kubernetes/helm/che/tiller-rbac.yaml`, { timeout: 10000 })
   }
 
-  checkTillerService() {
-    execa.shellSync('kubectl get services tiller-deploy -n kube-system', { timeout: 10000 })
+  async checkTillerService() {
+    await execa('kubectl', ['get', 'services', 'tiller-deploy', '-n', 'kube-system'], { timeout: 10000 })
   }
 
-  checkIfChePodIsCreated(flags: flags) {
+  async checkIfChePodIsCreated(flags: flags) {
     const k8s = require('@kubernetes/client-node')
     const kc = new k8s.KubeConfig()
     kc.loadFromDefault()
 
     const k8sApi = kc.makeApiClient(k8s.Core_v1Api)
 
-    k8sApi.listNamespacedPod(flags.chenamespace, undefined, undefined, undefined, undefined, 'app=che')
+    await k8sApi.listNamespacedPod(flags.chenamespace, undefined, undefined, undefined, undefined, 'app=che')
       .then(res => {
         res.body.items.forEach(pod => {
           console.log(`Pod name: ${pod.metadata.name}`)
@@ -125,7 +143,7 @@ export default class Up extends Command {
       }).catch(err => console.error(`Error: ${err.message}`))
   }
 
-  deployChe(flags: any) {
+  async deployChe(flags: any) {
     let command = `helm upgrade \\
                             --install che \\
                             --namespace ${flags.chenamespace} \\
@@ -133,6 +151,6 @@ export default class Up extends Command {
                             --set cheImage=eclipse/che-server:nightly \\
                             --set global.cheWorkspacesNamespace=${flags.chenamespace} \\
                             ${flags.chepath}/deploy/kubernetes/helm/che/`
-    execa.shellSync(command, { timeout: 10000 })
+    await execa.shell(command, { timeout: 10000 })
   }
 }
