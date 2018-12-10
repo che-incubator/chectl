af5ef2edea575a6bc4eb60ca69e25065cb371cdd
Artem Zatsarynnyi <azatsary@redhat.com>
2018-12-10 15:58:09 +0200

    Wait until a Tiller pod is running and ready to receive the requests instead of waiting 10 sec. Increase the timeout before terminating a task 10->20 sec.
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>

--- src/helpers/helm.ts
+++ src/helpers/helm.ts
@@ -32,8 +32,7 @@ export class HelmHelper {
   }
 
   async createTillerService() {
-    await execa('helm', ['init', '--service-account', 'tiller'], { timeout: 10000 })
-    await cli.wait(10000)
+    await execa('helm', ['init', '--service-account', 'tiller', '--wait'], { timeout: 20000 })
   }
 
 }
