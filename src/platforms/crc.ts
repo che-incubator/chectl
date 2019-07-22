8a03dfa73ec4973aa311dc6dbc2b51a6b99710c6
Florent Benoit <fbenoit@redhat.com>
2019-07-22 16:56:33 +0200

    feat(crc): Add support for CodeReady Containers It fixes https://github.com/eclipse/che/issues/13940
    Change-Id: If172e3b88df6f36aad09c1d69bab76ef5616fd2e
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ src/platforms/crc.ts
@@ -0,0 +1,79 @@
+/*********************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+// tslint:disable:object-curly-spacing
+
+import { Command } from '@oclif/command'
+import * as commandExists from 'command-exists'
+import * as execa from 'execa'
+import * as Listr from 'listr'
+
+/**
+ * Helper for Code Ready Container
+ */
+export class CRCHelper {
+  startTasks(flags: any, command: Command): Listr {
+    return new Listr([
+      {
+        title: 'Verify if oc is installed',
+        task: (_ctx: any, task: any) => {
+          if (!commandExists.sync('oc')) {
+            command.error('E_REQUISITE_NOT_FOUND')
+          } else {
+            task.title = `${task.title}...done.`
+          }
+        }
+      },
+      { title: 'Verify if crc is installed',
+        task: (_ctx: any, task: any) => {
+          if (!commandExists.sync('crc')) {
+            command.error('E_REQUISITE_NOT_FOUND', { code: 'E_REQUISITE_NOT_FOUND' })
+          } else {
+            task.title = `${task.title}...done.`
+          }
+        }
+      },
+      { title: 'Verify if CodeReady Containers is running',
+        task: async (_ctx: any, task: any) => {
+          const crcIsRunning = await this.isCRCRunning()
+          if (!crcIsRunning) {
+            command.error('E_PLATFORM_NOT_READY')
+          } else {
+            task.title = `${task.title}...done.`
+          }
+        }
+      },
+      { title: 'Retrieving CodeReady Containers IP and domain for routes URLs',
+        enabled: () => flags.domain !== undefined,
+        task: async (_ctx: any, task: any) => {
+          const ip = await this.getCRCIP()
+          flags.domain = ip + '.nip.io'
+          task.title = `${task.title}...${flags.domain}.`
+        }
+      },
+    ], {renderer: flags['listr-renderer'] as any})
+  }
+
+  async isCRCRunning(): Promise<boolean> {
+    const { code, stdout } = await execa('crc', ['status'], { timeout: 60000, reject: false })
+    if (code === 0 &&
+        stdout.includes('CRC VM:          Running') &&
+        stdout.includes('OpenShift:       Running')) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  async getCRCIP(): Promise<string> {
+    const { stdout } = await execa('crc', ['ip'], { timeout: 10000 })
+    return stdout
+  }
+
+}
