6c1c14f53259a19689bdcdf06a786bb3601ef025
Florent Benoit <fbenoit@redhat.com>
2019-08-14 17:20:17 +0200

    chore(release): Add scripts to make chectl release
    ./make-release.sh 7.0.0
    
    or to not care about system dependencies:
    
    ./run-script-in-docker.sh make-release.sh 7.0.0
    
    Change-Id: If291506c07c1fd04ecc99e3992ef9b9b518a608d
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ make-release.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+set -e
+set -u
+
+init() {
+  RED='\033[0;31m'
+  NC='\033[0m'
+}
+
+check() {
+  if [ $# -eq 0 ]; then
+    printf "%bError: %bNo script provided. Command is $ make-release.sh <version>\n" "${RED}" "${NC}"
+    exit 1
+  fi
+}
+
+apply_sed() {
+    SHORT_UNAME=$(uname -s)
+  if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' "$1" "$2"
+  elif [ "${SHORT_UNAME:0:5}" == "Linux" ]; then
+    sed -i "$1" "$2"
+  fi
+}
+run() {
+  # use master branch
+  git checkout master
+  
+  # reset local changes
+  while true; do
+    read -r -p "It will reset any local changes to the current branch ?" yn
+    case $yn in
+      [Yy]* ) break;;
+      [Nn]* ) exit;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+  
+  git fetch
+  if git show-ref -q --heads "release"; then
+    git branch -D release
+  fi
+  
+  VERSION=$1
+  
+  # Create VERSION file
+  echo "$VERSION" > VERSION
+  
+  # replace nightly versions by release version
+  apply_sed "s#eclipse/che-server:nightly#eclipse/che-server:${VERSION}#g" src/commands/server/start.ts
+  apply_sed "s#quay.io/eclipse/che-operator:nightly#quay.io/eclipse/che-operator:${VERSION}#g" src/commands/server/start.ts
+  
+  # now replace package.json dependencies
+  apply_sed "s;github.com/eclipse/che#\(.*\)\",;github.com/eclipse/che#${VERSION}\",;g" package.json
+  apply_sed "s;github.com/eclipse/che-operator#\(.*\)\",;github.com/eclipse/che-operator#${VERSION}\",;g" package.json
+  
+  # update yarn.lock
+  #yarn install
+  
+  # move into the release branch
+  git checkout -b release
+  
+  # add VERSION file to commit
+  git add VERSION src package.json
+}
+
+init "$@"
+check "$@"
+run "$@"
---
+++ run-script-in-docker.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+# git ROOT directory used to mount filesystem
+GIT_ROOT_DIRECTORY=$(git rev-parse --show-toplevel)
+
+# Container image
+IMAGE_NAME="che-incubator/chectl-docker-run"
+
+init() {
+  BLUE='\033[1;34m'
+  GREEN='\033[0;32m'
+  RED='\033[0;31m'
+  NC='\033[0m'
+  BOLD='\033[1m'
+}
+
+check() {
+  if [ $# -eq 0 ]; then
+    printf "%bError: %bNo script provided. Command is $ run-script-in-docker.sh <script-to-run> [optional-arguments-of-script-to-run]\n" "${RED}" "${NC}"
+    exit 1
+  fi
+  echo "check $1"
+  if [ ! -f "$1" ]; then
+    printf "%bError: %bscript %b provided is not existing. Command is $ run-script-in-docker.sh <script-to-run> [optional-arguments-of-script-to-run]\n" "${RED}" "${NC}" "${1}"
+    exit 1
+  fi
+}
+
+# Build image
+build() {
+  printf "%bBuilding image %b${IMAGE_NAME}${NC}..." "${BOLD}" "${BLUE}"
+  if docker build -t ${IMAGE_NAME} > docker-build-log 2>&1 -<<EOF
+  FROM alpine:3.10
+  RUN apk add --no-cache git bash
+  WORKDIR /workdir
+EOF
+then
+  printf "%b[OK]%b\n" "${GREEN}" "${NC}"
+  rm docker-build-log
+else
+  printf "%bFailure%b\n" "${RED}" "${NC}"
+  cat docker-build-log
+  exit 1
+fi
+}
+
+
+run() {
+  printf "%bRunning%b $*\n" "${BOLD}" "${NC}"
+  if docker run --rm -it -v "${GIT_ROOT_DIRECTORY}":/workdir --entrypoint=/bin/bash ${IMAGE_NAME} "$@"
+  then
+    printf "Script execution %b[OK]%b\n" "${GREEN}" "${NC}"
+  else
+    printf "%bFail to run the script%b\n" "${RED}" "${NC}"
+    exit 1
+  fi
+}
+
+init "$@"
+check "$@"
+build "$@"
+run "$@"
