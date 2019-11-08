38d88135bd9c87cb41a0d61c99c58ba04cb0e848
Tom George <tg82490@gmail.com>
2019-11-08 07:56:10 -0600

    feat: CheCtl start should *not* fix the docker images in the CR by default (#383)
    * Properly set che image tags based on release or nightly, and presence of cheimage parameter
    * Refactor image names into a constants file, check if they are different from the passed-in cheimage flag
    
    Signed-off-by: Tom George <tg82490@gmail.com>

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+const DEFAULT_CHE_IMAGE = 'eclipse/che-server:nightly'
+const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:nightly'
+
+export { DEFAULT_CHE_IMAGE, DEFAULT_CHE_OPERATOR_IMAGE }
