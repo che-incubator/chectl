53478d24b63b1a1375303bf759b8c6333f08c317
Mario Loriedo <mario.loriedo@gmail.com>
2019-11-08 19:12:14 +0100

    chore(release): release version 7.4.0
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const DEFAULT_CHE_IMAGE = 'eclipse/che-server:nightly'
-const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:nightly'
+const DEFAULT_CHE_IMAGE = 'eclipse/che-server:7.4.0'
+const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:7.4.0'
 
 export { DEFAULT_CHE_IMAGE, DEFAULT_CHE_OPERATOR_IMAGE }
