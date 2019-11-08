0604f35127b8f8fc3b168da66bcb3142469036f8
Tom George <tg82490@gmail.com>
2019-11-08 07:56:10 -0600

    feat: CheCtl start should *not* fix the docker images in the CR by default (#383)
    * Properly set che image tags based on release or nightly, and presence of cheimage parameter
    * Refactor image names into a constants file, check if they are different from the passed-in cheimage flag
    
    Signed-off-by: Tom George <tg82490@gmail.com>

--- src/commands/server/start.ts
+++ src/commands/server/start.ts
@@ -16,6 +16,7 @@ import * as notifier from 'node-notifier'
 import * as path from 'path'
 
 import { cheDeployment, cheNamespace, listrRenderer } from '../../common-flags'
+import { DEFAULT_CHE_IMAGE, DEFAULT_CHE_OPERATOR_IMAGE } from '../../constants'
 import { CheTasks } from '../../tasks/che'
 import { InstallerTasks } from '../../tasks/installers/installer'
 import { K8sTasks } from '../../tasks/platforms/k8s'
@@ -32,7 +33,7 @@ export default class Start extends Command {
     cheimage: string({
       char: 'i',
       description: 'Che server container image',
-      default: 'eclipse/che-server:7.3.1',
+      default: DEFAULT_CHE_IMAGE,
       env: 'CHE_CONTAINER_IMAGE'
     }),
     templates: string({
@@ -102,7 +103,7 @@ export default class Start extends Command {
     }),
     'che-operator-image': string({
       description: 'Container image of the operator. This parameter is used only when the installer is the operator',
-      default: 'quay.io/eclipse/che-operator:7.3.1'
+      default: DEFAULT_CHE_OPERATOR_IMAGE
     }),
     'che-operator-cr-yaml': string({
       description: 'Path to a yaml file that defines a CheCluster used by the operator. This parameter is used only when the installer is the operator.',
