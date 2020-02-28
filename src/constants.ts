89ee50a56f185f3fbbc2c6051b01c567f690ff4e
Mykola Morhun <mmorhun@redhat.com>
2020-02-28 14:35:44 +0200

    refactor: Move CA certificate generation job image dockerfile into separate repository (#546)
    * Move CA certificate generation job image dockerfile into separate repository
    
    Signed-off-by: Mykola Morhun <mmorhun@redhat.com>

--- src/constants.ts
+++ src/constants.ts
@@ -10,8 +10,10 @@
 
 export const DEFAULT_CHE_IMAGE = 'quay.io/eclipse/che-server:nightly'
 export const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:nightly'
-export const CA_CERT_GENERATION_JOB_IMAGE = 'quay.io/eclipse/che-cert-manager-ca-cert-generator:latest'
+
+// This image should be updated manually when needed.
+// Repository location: https://github.com/che-dockerfiles/che-cert-manager-ca-cert-generator-image
+export const CA_CERT_GENERATION_JOB_IMAGE = 'quay.io/eclipse/che-cert-manager-ca-cert-generator:671342c'
 
 export const CERT_MANAGER_NAMESPACE_NAME = 'cert-manager'
-
 export const CHE_TLS_SECRET_NAME = 'che-tls'
