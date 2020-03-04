607f26892ca8c79f9b7ead1c168ea701c6f407d3
flacatus <flacatus@redhat.com>
2020-03-04 20:44:14 +0100

    TEST
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .ci/cico_common.sh
+++ .ci/cico_common.sh
@@ -63,8 +63,8 @@ install_node_deps() {
 
 setup_kvm_machine_driver() {
   printInfo "Installing docker machine kvm drivers"
-  curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 -o /usr/bin/docker-machine-driver-kvm
-  chmod +x /usr/bin/docker-machine-driver-kvm
+  curl -L https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-centos7 -o /usr/local/bin/docker-machine-driver-kvm
+  chmod +x /usr/local/bin/docker-machine-driver-kvm
   check_libvirtd=$(systemctl is-active libvirtd)
   if [ $check_libvirtd != 'active' ]; then
     virsh net-start default
@@ -74,7 +74,7 @@ setup_kvm_machine_driver() {
 minishift_installation() {
   printInfo "Downloading Minishift binaries"
   curl -L https://github.com/minishift/minishift/releases/download/v$MSFT_RELEASE/minishift-$MSFT_RELEASE-linux-amd64.tgz \
-    -o ${CHECTL_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar && tar -xvf ${CHECTL_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar -C /usr/bin --strip-components=1
+    -o ${CHECTL_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar && tar -xvf ${CHECTL_REPO}/tmp/minishift-$MSFT_RELEASE-linux-amd64.tar -C /usr/local/bin --strip-components=1
   echo "[INFO] Starting a new OC cluster."
   minishift profile set ${PROFILE}
   minishift start --memory=${RAM_MEMORY} && eval $(minishift oc-env)
