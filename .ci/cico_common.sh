0f38d75d128ffd45545ddef2fda43e7a6b7aba5e
flacatus <flacatus@redhat.com>
2020-03-04 20:52:45 +0100

    TEST
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .ci/cico_common.sh
+++ .ci/cico_common.sh
@@ -58,7 +58,7 @@ start_libvirt() {
 install_node_deps() {
   curl -sL https://rpm.nodesource.com/setup_10.x | bash -
   yum-config-manager --add-repo https://dl.yarnpkg.com/rpm/yarn.repo
-  yum install -y nodejs yarn
+  yum install -y nodejs yarn git
 }
 
 setup_kvm_machine_driver() {
