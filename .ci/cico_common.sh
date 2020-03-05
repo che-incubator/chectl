6129bdbede12222f93b0acabee7b2b6e1b135744
flacatus <flacatus@redhat.com>
2020-03-05 17:32:48 +0100

    TEST
    Signed-off-by: flacatus <flacatus@redhat.com>

--- .ci/cico_common.sh
+++ .ci/cico_common.sh
@@ -107,8 +107,10 @@ minikube_installation() {
   curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
     && chmod +x minikube
 
-  cp minikube /usr/local/bin && rm minikube
+  sudo cp minikube /usr/local/bin && rm minikube
   export CHANGE_MINIKUBE_NONE_USER=true
+  adduser chectl
+  su - chectl
   minikube start --memory=8192 --force
 
 
