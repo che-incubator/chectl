7e5acb884e19240b4b8838de101130d97e172747
Anatolii Bazko <abazko@redhat.com>
2020-03-09 12:20:01 +0200

    chore(release): release version 7.9.1 (#566)
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const DEFAULT_CHE_IMAGE = 'quay.io/eclipse/che-server:7.9.0'
-const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:7.9.0'
+const DEFAULT_CHE_IMAGE = 'quay.io/eclipse/che-server:7.9.1'
+const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:7.9.1'
 
 export { DEFAULT_CHE_IMAGE, DEFAULT_CHE_OPERATOR_IMAGE }
