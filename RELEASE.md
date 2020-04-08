11b96d8421b7bd010249cadf767729d84b17ce3b
Anatoliy Bazko <abazko@redhat.com>
2020-04-08 09:41:56 +0300

    Fixes
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- RELEASE.md
+++ RELEASE.md
@@ -1,7 +1,9 @@
 # `chectl` release process
 
+1. Launch release script
+
 ```bash
 ./make-release.sh ${RELEASE} ${BRANCH_NAME}
 ```
 
-Track [TravisCI Job](https://travis-ci.org/che-incubator/chectl/branches)
+2. Wait until [TravisCI Job](https://travis-ci.org/che-incubator/chectl/branches) from `release` branch is completed
--- make-release.sh
+++ make-release.sh
@@ -14,37 +14,16 @@ set -e
 set -u
 
 init() {
-  RED='\e[31m'
-  NC='\e[0m'
-  YELLOW='\e[33m'
-  GREEN='\e[32m'
-
   RELEASE="$1"
-  BRANCH=${2:-master}
+  BRANCH=$(echo $RELEASE | sed 's/.$/x/')
   GIT_REMOTE_UPSTREAM="git@github.com:che-incubator/chectl.git"
-  CURRENT_DIR=$(pwd)
-  BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 }
 
 check() {
-  if [ $# -lt 1 ]; then
-    printf "%bError: %bWrong number of parameters.\nUsage: ./make-release.sh <version>\n" "${RED}" "${NC}"
+  if [[ $# -lt 1 ]]; then
+    echo "[ERROR] Wrong number of parameters.\nUsage: ./make-release.sh <version>"
     exit 1
   fi
-
-  echo "Release '$RELEASE' from branch '$BRANCH'"
-}
-
-ask() {
-  while true; do
-    echo -e -n $GREEN$@$NC" (Y)es or (N)o "
-    read -r yn
-    case $yn in
-      [Yy]* ) return 0;;
-      [Nn]* ) return 1;;
-      * ) echo "Please answer (Y)es or (N)o. ";;
-    esac
-  done
 }
 
 apply_sed() {
@@ -56,98 +35,73 @@ apply_sed() {
   fi
 }
 
-resetLocalChanges() {
-  set +e
-  ask "1. Reset local changes?"
-  result=$?
-  set -e
+resetChanges() {
+  echo "[INFO] Reset changes in $1 branch"
+  git reset --hard
+  git checkout $1
+  git fetch ${GIT_REMOTE_UPSTREAM} --prune
+  git pull ${GIT_REMOTE_UPSTREAM} $1
+}
 
-  if [[ $result == 0 ]]; then
-    git reset --hard
-    git checkout $BRANCH
-    git fetch ${GIT_REMOTE_UPSTREAM} --prune
-    git pull ${GIT_REMOTE_UPSTREAM} $BRANCH
-    git checkout -B $RELEASE
-  elif [[ $result == 1 ]]; then
-    echo -e $YELLOW"> SKIPPED"$NC
+checkoutToReleaseBranch() {
+  echo "[INFO] Checking out to $BRANCH branch."
+  local branchExist=$(git ls-remote -q --heads | grep $BRANCH | wc -l)
+  if [[ $branchExist == 1 ]]; then
+    echo "[INFO] $BRANCH exists."
+    resetChanges $BRANCH
+  else
+    echo "[INFO] $BRANCH does not exist. Will be created a new one from master."
+    resetChanges master
+    git push origin master:$BRANCH
   fi
+  git checkout -B $RELEASE
 }
 
 release() {
-  set +e
-  ask "2. Release?"
-  result=$?
-  set -e
+  echo "[INFO] Releasing a new $RELEASE version"
 
-  if [[ $result == 0 ]]; then
-    # Create VERSION file
-    echo "$RELEASE" > VERSION
+  # Create VERSION file
+  echo "$RELEASE" > VERSION
 
-    # replace nightly versions by release version
-    apply_sed "s#quay.io/eclipse/che-server:nightly#quay.io/eclipse/che-server:${RELEASE}#g" src/constants.ts
-    apply_sed "s#quay.io/eclipse/che-operator:nightly#quay.io/eclipse/che-operator:${RELEASE}#g" src/constants.ts
+  # replace nightly versions by release version
+  apply_sed "s#q2uay.io/eclipse/che-server:nightly#quay.io/eclipse/che-server:${RELEASE}#g" src/constants.ts
+  apply_sed "s#quay.io/eclipse/che-operator:nightly#quay.io/eclipse/che-operator:${RELEASE}#g" src/constants.ts
 
-    # now replace package.json dependencies
-    apply_sed "s;github.com/eclipse/che#\(.*\)\",;github.com/eclipse/che#${RELEASE}\",;g" package.json
-    apply_sed "s;github.com/eclipse/che-operator#\(.*\)\",;github.com/eclipse/che-operator#${RELEASE}\",;g" package.json
+  # now replace package.json dependencies
+  apply_sed "s;github.com/eclipse/che#\(.*\)\",;github.com/eclipse/che#${RELEASE}\",;g" package.json
+  apply_sed "s;github.com/eclipse/che-operator#\(.*\)\",;github.com/eclipse/che-operator#${RELEASE}\",;g" package.json
 
-    # build
-    yarn
-    yarn pack
-    yarn test
-  elif [[ $result == 1 ]]; then
-    echo -e $YELLOW"> SKIPPED"$NC
-  fi
+  # build
+  yarn
+  yarn pack
+  yarn test
 }
 
 commitChanges() {
-  set +e
-  ask "3. Commit changes?"
-  result=$?
-  set -e
-
-  if [[ $result == 0 ]]; then
-    git add -A
-    git commit -s -m "chore(release): release version ${RELEASE}"
-  elif [[ $result == 1 ]]; then
-    echo -e $YELLOW"> SKIPPED"$NC
-  fi
+  echo "[INFO] Pushing changes to $RELEASE branch"
+  git add -A
+  git commit -s -m "chore(release): release version ${RELEASE}"
+  git push origin $RELEASE
 }
 
-pushChanges() {
-  set +e
-  ask "4. Push changes?"
-  result=$?
-  set -e
-
-  if [[ $result == 0 ]]; then
-    git push origin $RELEASE
-  elif [[ $result == 1 ]]; then
-    echo -e $YELLOW"> SKIPPED"$NC
-  fi
+createReleaseBranch() {
+  echo "[INFO] Create the release branch based on $RELEASE"
+  git push origin $RELEASE:release -f
 }
 
-pushChangesToReleaseBranch() {
-  set +e
-  ask "5. Push changes to release branch?"
-  result=$?
-  set -e
-
-  if [[ $result == 0 ]]; then
-    git push origin $RELEASE:release -f
-  elif [[ $result == 1 ]]; then
-    echo -e $YELLOW"> SKIPPED"$NC
-  fi
+createPR() {
+  echo "[INFO] Creating a PR"
+  hub pull-request --base ${BRANCH} --head ${RELEASE} --browse -m "Release version ${RELEASE}"
 }
 
 run() {
-  resetLocalChanges
+  checkoutToReleaseBranch
   release
   commitChanges
-  pushChanges
-  pushChangesToReleaseBranch
+  createReleaseBranch
+  createPR
 }
 
-init "$@"
-check "$@"
-run "$@"
+init $@
+check $@
+run $@
