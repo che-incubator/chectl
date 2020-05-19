7be1673ce7eba958eeb0f8da04e81832b6ba8301
Anatoliy Bazko <abazko@redhat.com>
2020-05-19 11:56:15 +0300

    Fix
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- src/util.ts
+++ src/util.ts
@@ -10,6 +10,8 @@
 
 import * as commandExists from 'command-exists'
 
+import { DEFAULT_CHE_OPERATOR_IMAGE } from './constants'
+
 export const KUBERNETES_CLI = 'kubectl'
 export const OPENSHIFT_CLI = 'oc'
 
@@ -69,7 +71,7 @@ export function base64Decode(arg: string): string {
  * Indicates if stable version of `chectl` is used.
  */
 export function isStableVersion(flags: any): boolean {
-  const operatorImage = flags['che-operator-image']
+  const operatorImage = flags['che-operator-image'] || DEFAULT_CHE_OPERATOR_IMAGE
   const cheVersion = operatorImage.split(':')[1]
   return cheVersion !== 'nightly' && cheVersion !== 'latest'
 }
