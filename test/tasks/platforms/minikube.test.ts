2e41c0368ac1b375a780571c2b425a9897ce294c
Florent Benoit <fbenoit@redhat.com>
2020-08-12 11:01:07 +0200

    fix(minikube): do not enable ingress addon if already enabled new output of addons list command is returning pipe characters instead of : as separator thus the check is failing use new json output to make it more reliable (if json output is available)
    Change-Id: Ifd0c8f716c42ecc2c412ba8d6964e3e22b8d93f5
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- test/tasks/platforms/minikube.test.ts
+++ test/tasks/platforms/minikube.test.ts
@@ -8,6 +8,8 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 import * as execa from 'execa'
+import * as fs from 'fs-extra'
+import * as path from 'path'
 import { expect, fancy } from 'fancy-test'
 
 import { MinikubeTasks } from '../../../src/tasks/platforms/minikube'
@@ -30,4 +32,34 @@ describe('start', () => {
       const res = await mh.isMinikubeRunning()
       expect(res).to.equal(false)
     })
+    fancy
+    .it('Check ingress addon is there with old minikube version without json output', async () => {
+      const v1JsonOutput = await fs.readFile(path.resolve(__dirname, 'minikube', 'minikube-addon-list-json-v1.output'), 'utf-8');
+      const v1Output = await fs.readFile(path.resolve(__dirname, 'minikube', 'minikube-addon-list.output'), 'utf-8');
+      ((execa as any) as jest.Mock).mockReturnValueOnce({ exitCode: 64, stderr: v1JsonOutput }).mockReturnValueOnce({ exitCode: 0, stdout: v1Output })
+      const res = await mh.isIngressAddonEnabled()
+      expect(res).to.equal(true)
+    })
+    fancy
+    .it('Check ingress addon is not there with old minikube version without json output', async () => {
+      const v1JsonOutput = await fs.readFile(path.resolve(__dirname, 'minikube', 'minikube-addon-list-json-v1.output'), 'utf-8');
+      const v1Output = await fs.readFile(path.resolve(__dirname, 'minikube', 'minikube-addon-list-no-ingress.output'), 'utf-8');
+      ((execa as any) as jest.Mock).mockReturnValueOnce({ exitCode: 64, stderr: v1JsonOutput }).mockReturnValueOnce({ exitCode: 0, stdout: v1Output })
+      const res = await mh.isIngressAddonEnabled()
+      expect(res).to.equal(false)
+    })    
+    fancy
+    .it('Check ingress addon is there with new minikube version with json output', async () => {
+      const v11JsonOutput = await fs.readFile(path.resolve(__dirname, 'minikube', 'minikube-addon-list-v11.json'), 'utf-8');
+      ((execa as any) as jest.Mock).mockReturnValueOnce({ exitCode: 0, stdout: v11JsonOutput })
+      const res = await mh.isIngressAddonEnabled()
+      expect(res).to.equal(true)
+    })
+    fancy
+    .it('Check ingress addon is not there with new minikube version with json output', async () => {
+      const v11JsonOutput = await fs.readFile(path.resolve(__dirname, 'minikube', 'minikube-addon-list-v11-not-present.json'), 'utf-8');
+      ((execa as any) as jest.Mock).mockReturnValueOnce({ exitCode: 0, stdout: v11JsonOutput })
+      const res = await mh.isIngressAddonEnabled()
+      expect(res).to.equal(false)
+    })    
 })
