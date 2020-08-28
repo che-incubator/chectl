96f93abcbb5e2a87bdd674fb837947bed3673bda
Anatolii Bazko <abazko@redhat.com>
2020-08-28 09:19:52 +0300

    Revert "chore(release): release version 7.18.0 (#823)" (#830)
    This reverts commit 37a834dcec9bf0f359e24147efb8bd9911619e96.

--- src/constants.ts
+++ src/constants.ts
@@ -8,8 +8,8 @@
  * SPDX-License-Identifier: EPL-2.0
  **********************************************************************/
 
-export const DEFAULT_CHE_IMAGE = 'quay.io/eclipse/che-server:7.18.0'
-export const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:7.18.0'
+export const DEFAULT_CHE_IMAGE = 'quay.io/eclipse/che-server:nightly'
+export const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:nightly'
 
 // This image should be updated manually when needed.
 // Repository location: https://github.com/che-dockerfiles/che-cert-manager-ca-cert-generator-image
