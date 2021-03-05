2980a70bd235ad8e716a092f3d6d7a81b66b7f2f
Flavius Lacatusu <flacatus@redhat.com>
2021-03-05 14:03:42 +0100

    chore(release): release version 7.27.0 (#1124)
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- src/constants.ts
+++ src/constants.ts
@@ -21,7 +21,7 @@ export const CHE_RELATED_COMPONENT_LABEL = 'client/org.eclipse.che=true'
 
 // images
 export const DEFAULT_CHE_OPERATOR_IMAGE_NAME = 'quay.io/eclipse/che-operator'
-export const DEFAULT_DEV_WORKSPACE_CONTROLLER_IMAGE = 'quay.io/devfile/devworkspace-controller:next'
+export const DEFAULT_DEV_WORKSPACE_CONTROLLER_IMAGE = 'quay.io/devfile/devworkspace-controller:v0.1.0'
 // This image should be updated manually when needed.
 // Repository location: https://github.com/che-dockerfiles/che-cert-manager-ca-cert-generator-image
 export const CA_CERT_GENERATION_JOB_IMAGE = 'quay.io/eclipse/che-cert-manager-ca-cert-generator:671342c'
