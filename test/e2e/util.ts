84a0c6083a12d4c59200213bf56e17b92f482c7d
Anatolii Bazko <abazko@redhat.com>
2022-02-02 11:56:23 +0200

    Fix
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- test/e2e/util.ts
+++ test/e2e/util.ts
@@ -20,7 +20,7 @@ import { CheHelper } from '../../src/api/che'
 import { CheGithubClient, TagInfo } from '../../src/api/github-client'
 import { KubeHelper } from '../../src/api/kube'
 import { OpenShiftHelper } from '../../src/api/openshift'
-import { CHE_CLUSTER_API_GROUP, CHE_CLUSTER_API_VERSION, DEFAULT_OLM_SUGGESTED_NAMESPACE } from '../../src/constants'
+import { DEFAULT_OLM_SUGGESTED_NAMESPACE } from '../../src/constants'
 
 // Fields which chectl returns for workspace:list commands
 interface WorkspaceInfo {
