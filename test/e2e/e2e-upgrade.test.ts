90bfdd502d43bf3d9dba215ade2d86c20843696d
Anatolii Bazko <abazko@redhat.com>
2022-02-15 12:46:44 +0200

    fix test
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- test/e2e/e2e-upgrade.test.ts
+++ test/e2e/e2e-upgrade.test.ts
@@ -16,31 +16,25 @@ import { E2eHelper, NAMESPACE } from './util'
 const helper = new E2eHelper()
 jest.setTimeout(1000000)
 
-const binChectl = E2eHelper.getChectlBinaries()
-
 const PLATFORM = process.env.PLATFORM || 'minikube'
-
 const INSTALLER = 'operator'
 
 const UPDATE_CHE_TIMEOUT_MS = 10 * 60 * 1000
 const CHE_VERSION_TIMEOUT_MS = 10 * 60 * 1000
 
 describe('Test Che upgrade', () => {
-  let cheVersion: string
-
   describe('Prepare latest stable Che', () => {
     it(`Deploy Che using ${INSTALLER} installer and self signed certificates`, async () => {
       // uses installed chectl (from a stable channel)
       // see github workflow
       const deployCommand = `chectl server:deploy --batch --platform=${PLATFORM} --installer=${INSTALLER} --chenamespace=${NAMESPACE} --telemetry=off`
       await helper.runCliCommand(deployCommand)
-
-      await helper.waitForVersionInCheCR(cheVersion, CHE_VERSION_TIMEOUT_MS)
     })
   })
 
   describe('Test Che update', () => {
     it('Update Eclipse Che Version', async () => {
+      const binChectl = E2eHelper.getChectlBinaries()
       await helper.runCliCommand(binChectl, ['server:update', '-y', `-n ${NAMESPACE}`, '--telemetry=off'])
       await helper.waitForCheServerImageTag(helper.getNewVersion(), UPDATE_CHE_TIMEOUT_MS)
     })
