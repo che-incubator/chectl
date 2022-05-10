c2cc9a856be68ec8b99b6460dbfac099c2e1312f
Anatolii Bazko <abazko@redhat.com>
2022-05-10 16:48:24 +0300

    feat: Allow to skip CertManager deployment
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- src/constants.ts
+++ src/constants.ts
@@ -13,18 +13,10 @@
 export const CHECTL_PROJECT_NAME = 'chectl'
 export const OPERATOR_TEMPLATE_DIR = 'che-operator'
 
-// minimal installable versions by current chectl
-export const MIN_OLM_INSTALLER_VERSION = '7.17.0'
-export const MIN_CHE_OPERATOR_INSTALLER_VERSION = '7.13.1'
-
-// labels
-export const CHE_RELATED_COMPONENT_LABEL = 'client/org.eclipse.che=true'
-
 // images
 export const DEFAULT_CHE_OPERATOR_IMAGE_NAME = 'quay.io/eclipse/che-operator'
 // This image should be updated manually when needed.
 // Repository location: https://github.com/che-dockerfiles/che-cert-manager-ca-cert-generator-image
-export const CA_CERT_GENERATION_JOB_IMAGE = 'quay.io/eclipse/che-cert-manager-ca-cert-generator:671342c'
 export const INDEX_IMG = 'quay.io/eclipse/eclipse-che-openshift-opm-catalog:next'
 export const DEV_WORKSPACE_NEXT_CATALOG_SOURCE_IMAGE = 'quay.io/devfile/devworkspace-operator-index:next'
 export const DEV_WORKSPACE_STABLE_CATALOG_SOURCE_IMAGE = 'quay.io/devfile/devworkspace-operator-index:release'
@@ -35,7 +27,6 @@ export const CERT_MANAGER_NAMESPACE_NAME = 'cert-manager'
 export const CHE_TLS_SECRET_NAME = 'che-tls'
 export const CHE_ROOT_CA_SECRET_NAME = 'self-signed-certificate'
 export const DEFAULT_CA_CERT_FILE_NAME = 'cheCA.crt'
-export const CHE_CLUSTER_CR_NAME = 'eclipse-che'
 
 // operator
 export const OPERATOR_DEPLOYMENT_NAME = 'che-operator'
@@ -51,7 +42,6 @@ export const CSV_PREFIX = 'eclipse-che'
 export const DEVWORKSPACE_CSV_PREFIX = 'devworkspace-operator'
 // OLM channels
 export const OLM_STABLE_CHANNEL_NAME = 'stable'
-export const OLM_STABLE_CHANNEL_STARTING_CSV_TEMPLATE = 'eclipse-che.v{{VERSION}}'
 export const OLM_NEXT_CHANNEL_NAME = 'next'
 // OLM namespaces
 export const DEFAULT_OPENSHIFT_MARKET_PLACE_NAMESPACE = 'openshift-marketplace'
@@ -69,10 +59,7 @@ export const OPENSHIFT_OLM_CATALOG = 'community-operators'
 // Documentation links
 export const DOC_LINK = 'https://www.eclipse.org/che/docs/'
 export const DOC_LINK_RELEASE_NOTES = ''
-export const DOCS_LINK_INSTALL_RUNNING_CHE_LOCALLY = 'https://www.eclipse.org/che/docs/che-7/installation-guide/installing-che-locally/'
 export const DOCS_LINK_IMPORT_CA_CERT_INTO_BROWSER = 'https://www.eclipse.org/che/docs/che-7/end-user-guide/importing-certificates-to-browsers/'
-export const DOCS_LINK_HOW_TO_ADD_IDENTITY_PROVIDER_OS4 = 'https://docs.openshift.com/container-platform/latest/authentication/understanding-identity-provider.html#identity-provider-overview_understanding-identity-provider'
-export const DOCS_LINK_HOW_TO_CREATE_USER_OS3 = 'https://docs.openshift.com/container-platform/3.11/install_config/configuring_authentication.html'
 export const DOC_LINK_CONFIGURE_API_SERVER = 'https://kubernetes.io/docs/reference/access-authn-authz/authentication/#configuring-the-api-server'
 
 export const OUTPUT_SEPARATOR = '-------------------------------------------------------------------------------'
