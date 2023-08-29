82e69423865b963014ae9a29df85ade4dd356866
Anatolii Bazko <abazko@redhat.com>
2023-08-29 09:24:45 +0200

    fix typo
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- .ci/oc-common.sh
+++ .ci/oc-common.sh
@@ -35,8 +35,8 @@ installNodeVersion() {
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  nvm install ${version
-  nvm use ${version
+  nvm install ${version}
+  nvm use ${version}
 }
 
 collectEclipseCheLogs() {
