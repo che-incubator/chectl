8c553e73b3702ee8959b5fae8ee1398924aba2cf
Anatolii Bazko <abazko@redhat.com>
2026-07-20 13:11:45 +0200

    chore(deps): Bump execa from 5.1.1 to 10.0.0
    Signed-off-by: Anatolii Bazko <abazko@redhat.com>

--- test/mocks/execa.ts
+++ test/mocks/execa.ts
@@ -1,5 +1,13 @@
 /**
- * Mock for execa module to support ES module in Jest/CommonJS environment
+ * Copyright (c) 2019-2026 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
  */
 
 export interface ExecaReturnValue {
